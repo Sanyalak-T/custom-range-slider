638787bb33fa73ad4470be14c24b65a6018674f3
Sanyalak-T <isanyalakt@gmail.com>
2021-11-25 18:31:30 +0700

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const codes = document.querySelectorAll('.code')
+const range = document.getElementById('range')
 
-codes[0].focus()
+range.addEventListener('input', (e) => {
+    const value = +e.target.value
+    const label = e.target.nextElementSibling
 
-codes.forEach((code, idx) => {
-    code.addEventListener('keydown', (e) => {
-        codes[idx].value = ''
-        if(e.key >= 0 && e.key <= 9) {
-            setTimeout(() => codes[idx + 1].focus(), 10)
-        }else if(e.key === 'Backspace') {
-            setTimeout(() => codes[idx - 1].focus(), 10)
-        }
-    })
+    const range_width = getComputedStyle(e.target).getPropertyValue('width')
+    const label_width = getComputedStyle(label).getPropertyValue('width')
+
+    const num_width = +range_width.substring(0, range_width.length - 2)
+    const num_label_width = +label_width.substring(0, label_width.length - 2)
+
+    const max = +e.target.max
+    const min = +e.target.min
+
+    const left = value * (num_width / max) - num_label_width / 2 + scale(value, min, max, 10, -10)
+
+    label.style.left = `${left}px`
+
+    label.innerHTML = value
 })
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
